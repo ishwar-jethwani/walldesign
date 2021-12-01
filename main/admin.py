1cc35dd51caa07ceb3c6693334d52abfb2dffae8
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-12-01 22:33:03 +0530

    final

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
 from .models import *
-admin.site.register[Header,ProjectImage,Category,Testimonial,ProjectImage,Product,Projects,Address,Feadback,Contact,Services]
+admin.site.register([Header,ProjectImage,Category,Testimonial,ProjectImage,Product,Projects,Address,Feadback,Contact,Services])
