37adff063b69b58a2c0faed71e31182b37389465
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-12-01 22:30:13 +0530

    final

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register[Header,ProjectImage,Category,Testimonial,ProjectImage,Product,Projects,Address,Feadback,Contact,Services]
