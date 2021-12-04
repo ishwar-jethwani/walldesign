3820a0dfd49bb6e41312cad0aac1ce317c73e097
wolfpackproject <86668915+wolfpackproject@users.noreply.github.com>
2021-12-04 20:33:34 +0530

    finaL

--- main/views.py
+++ main/views.py
@@ -3,14 +3,22 @@ from django.shortcuts import render
 from .models import *
 
 def home(request):
+
     context = {
         "headers":Header.objects.all()[:1],
         "categories":Category.objects.all(),
         "projects": Projects.objects.all(),
         "testimonials":Testimonial.objects.all(),
         "address":Address.objects.all()[:1],
+        "product_first":Product.objects.all()[:1],
+        "products":Product.objects.all()[1:]
 
     }
+
+
+
+
+    
 
     if request.method == "POST":
         email = request.GET.get("sub_email")
